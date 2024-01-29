8617817394c4629dfd6c39f1403a305059be805c
aashishthakurcodes <j9183208@gmail.com>
2024-01-29 17:21:09 +0530

    Utils

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@
 import dotenv from 'dotenv'
 import express from "express";
 import connectDB from "./db/db.js";
+import { app } from './app.js';
+// const app=express();
 
 dotenv.config({path:'./env'})
 
-connectDB();
+connectDB().then(()=>{
+    
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at PORT : ${process.env.PORT}`)
+    })
+}).catch((error)=>{
+    console.log("Error in gotting Connection",error)
+})
