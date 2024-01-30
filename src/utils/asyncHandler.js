33f0f3ac19f6f9f9cdbb0a924adf0fc0128c7ba5
aashishthakurcodes <j9183208@gmail.com>
2024-01-30 18:32:02 +0530

    err/res utilis

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,18 @@
-const asyncHandler=()=>{
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req,res, next)).catch((err)=>next(err))
+  };
+};
 
-}
+export { asyncHandler };
 
-export {asyncHandler}
+// const asyncHandlers =(function) =>async(res,req,next)=>{
+// try {
+//     await function(req,res,next)
+// } catch (error) {
+//     res.status(error.code || 500).json({
+// success:false,
+// messsage:error.messsage
+//     })
+// }
+// }
