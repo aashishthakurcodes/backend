425924135c126c811bbba0f3869ecf63255d10c7
aashishthakurcodes <j9183208@gmail.com>
2024-02-15 13:15:02 +0530

    Logout err

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -171,22 +171,30 @@ const loginUser = asyncHandler(async (req, res) => {
     );
 });
 //LOgout User
-const logoutUser = asyncHandler(async (req, res) => {
-  await User.findByIdAndUpdate(req.user._id, {
-    $set: {
-      refreshToken: undefined,
-    },
-    new: true,
-  });
-  const options = {
-    httpOnly: true,
-    secure: true,
-  };
-  return res.status(200)
-  .clearCookie("accessToken",options)
-  .clearCookie("refreshToken",options)
-  .json(new ApiResponse(200,{},"User Logged Out Succesfully"))
 
-});
+const logoutUser = asyncHandler(async(req, res) => {
+  await User.findByIdAndUpdate(
+      req.user._id,
+      {
+          $unset: {
+              refreshToken: 1 // this removes the field from document
+          }
+      },
+      {
+          new: true
+      }
+  )
+
+  const options = {
+      httpOnly: true,
+      secure: true
+  }
+
+  return res
+  .status(200)
+  .clearCookie("accessToken", options)
+  .clearCookie("refreshToken", options)
+  .json(new ApiResponse(200, {}, "User logged Out"))
+})
 
 export { registerUser, loginUser, logoutUser };
